018813d0cc530dc3e21981d8e2e5a0c3c132cf78
Sam <samuel.rac@telecom-sudparis.eu>
2019-05-15 19:51:32 +0200

    script prise de photo

--- src/picture.sh
+++ src/picture.sh
@@ -4,5 +4,6 @@ echo $1
 if [ ! -e tmp ]; then
     mkdir ./tmp
 fi
-v4l2-ctl -d /dev/video2 --set-ctrl=saturation=200
-ffmpeg -f video4linux2 -s 1920x1080 -i /dev/video2 -vframes 1 ./tmp/$1.jpg
+#v4l2-ctl -d /dev/video2 --set-ctrl=saturation=200
+v4l2-ctl -d /dev/video0 --set-ctrl=saturation=128
+ffmpeg -f video4linux2 -s 1920x1080 -i /dev/video0 -vframes 1 ./tmp/$1.jpg
