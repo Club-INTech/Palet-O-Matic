b997a3ecc70e3d257ec5e551906cad23c83cf5f1
Sam <samuel.rac@telecom-sudparis.eu>
2019-05-18 15:55:33 +0200

    ajout de config.py

---
+++ src/config.py
@@ -0,0 +1,17 @@
+COULEUR = "purple"
+# COULEUR = "yellow"
+
+# Affichage du debug.
+DEBUG = False
+DEBUG_PLOT = False
+
+COTE_CALE_MM = 330
+COTE_CALE_PX = 1080
+
+# Position du centre de la cale violet.
+X_CHAOS_PURPLE = 500
+Y_CHAOS_PURPLE = 1050
+
+# Position du centre de la cale coté jaune.
+X_CHAOS_YELLOW = 500
+Y_CHAOS_YELLOW = 1050
